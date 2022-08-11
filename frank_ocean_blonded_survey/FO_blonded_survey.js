e235d3c4432d581e9b30e5eeb4b8f682ef743f6f
Mokgase Mmakola <78242011+Mokgase@users.noreply.github.com>
2022-08-11 15:35:09 +0200

    Add files via upload

---
+++ frank_ocean_blonded_survey/FO_blonded_survey.js
@@ -0,0 +1,7 @@
+// const clearSelection = (name) =>{
+//     const radioBtns =document.querySelectorAll("input[type='radio'][name' "+name+"']");
+//     radioBtns.forEach((radioBtn) =>{
+//         if(radioBtn.checked === true)
+//         radioBtn.checked = false;
+//     });
+// }
